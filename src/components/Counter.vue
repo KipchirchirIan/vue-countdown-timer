8d456855853e13ffef5c8158ca81e77cbbe16bbc
Ian Kipchirchir <potterke4@gmail.com>
2021-04-06 13:14:07 +0300

    Add component to manage counter

---
+++ src/components/Counter.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ days }}
+    {{ hours }}
+    {{ minutes }}
+    {{ seconds }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+  data () {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
